f4c61024d669b5be643b67b89d7dbdf15798612d
JeongUk Park <jeongph.dev@gmail.com>
2026-04-23 16:57:45 +0900

    test: Testcontainers 기반 통합 테스트 베이스 클래스 추가

---
+++ src/test/java/com/artinus/ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.artinus;
+
+import com.artinus.support.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class ApplicationTests extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ src/test/java/com/artinus/support/AbstractIntegrationTest.java
@@ -0,0 +1,21 @@
+package com.artinus.support;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+public abstract class AbstractIntegrationTest {
+
+    @Container
+    @ServiceConnection
+    static final MySQLContainer<?> MYSQL = new MySQLContainer<>("mysql:8.0")
+            .withDatabaseName("artinus")
+            .withUsername("artinus")
+            .withPassword("artinus");
+}
