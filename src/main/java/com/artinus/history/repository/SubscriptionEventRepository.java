cd0063c1ab22fd34b5699f5c93ed944367fd246d
JeongUk Park <jeongph.dev@gmail.com>
2026-04-23 17:53:11 +0900

    feat(repository): Subscription 및 SubscriptionEvent JPA 레포지토리 추가

---
+++ src/main/java/com/artinus/history/repository/SubscriptionEventRepository.java
@@ -0,0 +1,11 @@
+package com.artinus.history.repository;
+
+import com.artinus.history.domain.SubscriptionEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SubscriptionEventRepository extends JpaRepository<SubscriptionEvent, Long> {
+
+    List<SubscriptionEvent> findByPhoneNumberOrderByOccurredAtDesc(String phoneNumber);
+}
